c12435089e28afdf39c8f081cea85bb8be88c21c
Renan Betereli <renan@mantec.tech>
2024-11-07 18:55:36 -0300

    Implemented new array test and performance test

--- src/test/java/br/com/betereli/ArraysCompareTest.java
+++ src/test/java/br/com/betereli/ArraysCompareTest.java
@@ -1,14 +1,38 @@
 package br.com.betereli;
 
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assume.assumeNotNull;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
 class ArraysCompareTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@Timeout(value = 15, unit = TimeUnit.MILLISECONDS)
+	void testSort() {
+		int [] numbers = {25,8,21,32,3};
+		int [] excpectedArray = {3,8,21,25,32};
+		
+		Arrays.sort(numbers);
+		
+		assertArrayEquals(numbers, excpectedArray);
+	}
+	
+	@Test
+	@Timeout(1)
+	void testSortPerformance() {
+		int [] numbers = {25,8,21,32,3};
+		//int [] excpectedArray = {3,8,21,25,32};
+		for(int i = 0; i < 100000000; i++) {
+			numbers[0]=i;
+			Arrays.sort(numbers);
+		}
+		//assertArrayEquals(numbers, excpectedArray);
 	}
 
 }
